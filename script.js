bb7d99ac6d9f62c7197451a543bac4ecb508ab3b
Ashton Walden <ashtonwalden@icloud.com>
2020-04-09 22:18:19 -0600

    Cleaned up code

--- script.js
+++ script.js
@@ -4,36 +4,41 @@ var generateBtn = document.querySelector("#generate");
 var promptPasswordLength = prompt("How long would you like your pass word to be? (8-128 characters)");
 var passwordLength = parseInt(promptPasswordLength); 
 
+passwordPrompt();
 
-if (passwordLength < 8){
-    alert("Password too short. Please try again.");
-}
-else if (passwordLength > 128){
-    alert("Password too long. Please try again.");
-}
-else {
-    var confirmLowerCase = confirm("Would you like lower case letters to be included?");
-    var confirmUpperCase = confirm("Would you like upper case letters to be included?");
-    var confirmNumbers = confirm("Would you like numbers letters to be included?");
-    var confirmSpecialCharacters = confirm("Would you like special characters letters to be included?");
+function passwordPrompt (){
+    if (passwordLength < 8){
+        alert("Password too short. Please try again.");
+        onreset(passwordPrompt);
+    }
+    else if (passwordLength > 128){
+        alert("Password too long. Please try again.");
+        onreset(passwordPrompt);
+    }  
+    else {
+        var confirmLowerCase = confirm("Would you like lower case letters to be included?");
+        var confirmUpperCase = confirm("Would you like upper case letters to be included?");
+        var confirmNumbers = confirm("Would you like numbers letters to be included?");
+        var confirmSpecialCharacters = confirm("Would you like special characters letters to be included?");
 
-    var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", 
-    "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "L", 
-    "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-    var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "~", "?", "<", ">", "+"];
+        var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", 
+        "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+        var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "L", 
+        "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+        var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+        var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "~", "?", "<", ">", "+"];
 
-    var passwordComponentsArray = []
-    function passwordComponents (){
-        if (confirmLowerCase)
-            passwordComponentsArray = passwordComponentsArray.concat(lowerCase);
-        if (confirmUpperCase)
-            passwordComponentsArray = passwordComponentsArray.concat(upperCase);
-        if (confirmNumbers)
-            passwordComponentsArray = passwordComponentsArray.concat(numbers);
-        if (confirmSpecialCharacters)
-            passwordComponentsArray = passwordComponentsArray.concat(specialCharacters);
+        var passwordComponentsArray = []
+        function passwordComponents (){
+            if (confirmLowerCase)
+                passwordComponentsArray = passwordComponentsArray.concat(lowerCase);
+            if (confirmUpperCase)
+                passwordComponentsArray = passwordComponentsArray.concat(upperCase);
+            if (confirmNumbers)
+                passwordComponentsArray = passwordComponentsArray.concat(numbers);
+            if (confirmSpecialCharacters)
+                passwordComponentsArray = passwordComponentsArray.concat(specialCharacters);
+    };
 };
 
 passwordComponents();
